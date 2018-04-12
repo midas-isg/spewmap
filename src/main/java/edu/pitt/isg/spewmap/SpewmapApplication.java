aef35e6f002de6f17617dab24e98b182a3aefc5f
sahawut <sahawut@gmail.com>
2018-04-12 13:09:02 -0400

    Added generated files from https://start.spring.io/: Dependencies= Actuator, DevTools, JPA, Lombok, PostgreSQL, Thymeleaf, Web

---
+++ src/main/java/edu/pitt/isg/spewmap/SpewmapApplication.java
@@ -0,0 +1,12 @@
+package edu.pitt.isg.spewmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpewmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpewmapApplication.class, args);
+	}
+}
