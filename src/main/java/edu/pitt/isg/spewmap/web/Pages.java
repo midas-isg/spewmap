c34f1543754638b23592a1ecac23ddc63eef003e
sahawut <sahawut@gmail.com>
2018-06-29 09:10:46 -0400

    No more default page.

--- src/main/java/edu/pitt/isg/spewmap/web/Pages.java
+++ src/main/java/edu/pitt/isg/spewmap/web/Pages.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class Pages {
-    @GetMapping()
+    @GetMapping("/")
     public String index(){
         return "index";
     }
