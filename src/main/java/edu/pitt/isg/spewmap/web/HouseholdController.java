57d866b6b7bfec3784373bbb5162945d3826cfbe
Sahawut <sahawut@gmail.com>
2018-11-15 09:14:54 -0500

    Rolled back to /spewmap/households/api

--- src/main/java/edu/pitt/isg/spewmap/web/HouseholdController.java
+++ src/main/java/edu/pitt/isg/spewmap/web/HouseholdController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api/households")
+@RequestMapping("/households/api")
 @RequiredArgsConstructor
 @Slf4j
 public class HouseholdController {
