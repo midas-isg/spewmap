5503a4a094cf91730b50a985218bd0a43a031c97
Sahawut <sahawut@gmail.com>
2019-02-27 08:57:02 -0500

    Used SSE & EventSource to avoid proxy timeout.

--- src/main/java/edu/pitt/isg/spewmap/web/HouseholdController.java
+++ src/main/java/edu/pitt/isg/spewmap/web/HouseholdController.java
@@ -4,16 +4,23 @@ import com.google.gson.Gson;
 import edu.pitt.isg.spewmap.spe.HouseholdRule;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
 import org.springframework.data.mongodb.core.geo.GeoJsonMultiPolygon;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.stream.Stream;
 
 import static java.time.Duration.ofMillis;
@@ -26,41 +33,51 @@ import static org.springframework.http.MediaType.*;
 @Slf4j
 public class HouseholdController {
     final HouseholdRule rule;
+    Map<Integer, Mono<String>> id2mono = new HashMap<>();
+    int id = 0;
 
     @PostMapping("/summarize")
-    public Object summarize(@RequestBody GeoJsonMultiPolygon multiPolygon){
-        return rule.summarize(multiPolygon);
+    public Mono<String> summarize(@RequestBody GeoJsonMultiPolygon multiPolygon){
+        id2mono.put(++id, rule.summarize(multiPolygon).map(this::toJson));
+        return Mono.just("redirect:stream/" + id);
     }
 
-    @GetMapping(value = "/stream", produces = TEXT_EVENT_STREAM_VALUE)
-    public Object stream(){
-        return flux();
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNoSuchElementException() {
+        return "Not Found";
     }
 
-    private Flux<String> flux() {
-        Flux<String> slow = fluxSlow();
+    @GetMapping(value = "/stream/{id}", produces = TEXT_EVENT_STREAM_VALUE)
+    public Object stream(@PathVariable Integer id){
+        Mono<String> mono = id2mono.get(id);
+        if (id == null || mono == null)
+            throw new NoSuchElementException("id = " + id);
+        Flux<String> flux = flux(mono);
+        id2mono.remove(id);
+        return flux;
+    }
+
+    private Flux<String> flux(Publisher<String> slow) {
         return Flux.from(fluxInfinity())
                 .takeUntilOther(slow)
                 .concatWith(slow);
     }
 
-    Flux<String> fluxSlow() {
-        return Flux.just("Done").delayElements(ofMillis(100))
-//                .log()
-                ;
-    }
-    Flux<String> fluxInfinity() {
-        ;
-
+    private Flux<String> fluxInfinity() {
         return Flux.fromStream(Stream.generate(() -> waitMessage()))
-                .delayElements(ofMillis(10))
-//                .log()
+                .delayElements(ofMillis(10_000))//.log()
                 ;
     }
 
     private String  waitMessage() {
-        Map<String, String> map = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
         map.put("message", "Querying please wait.");
+        map.put("pending", true);
+        return toJson(map);
+    }
+
+    private String toJson(Object map) {
         return new Gson().toJson(map);
     }
 }
